48a9a38ae0e307ad44aa43f026f61ad43d8ce81c
zplusfour <zplusfour.node@outlook.com>
2020-12-27 17:09:08 +0200

    aadded aardvark bin env path

--- env.ps1
+++ env.ps1
@@ -2,4 +2,4 @@ $env:Path += ";C:\MinGW\bin" # MinGW Setup
 $env:Path += ";C:\ProgramData\chocoportable" # Chocolatey
 $env:Path += ";C:\ProgramData\chocolatey\bin" # Make command
 $env:Path += ";C:\tools\git\bin" # Git
-$env:Path += ";~\desktop\aardvark\bin" # Aardvark
+$env:Path += ";C:\Aardvark\aardvark" # Aardvark
