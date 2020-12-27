1b15f730ddfa23b8758a1f1d48f099f0b1ab1bb8
zplusfour <zplusfour.node@outlook.com>
2020-12-27 18:01:53 +0200

    Added README image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # The Aardvark Programming Language
 
-description later
+![Aardvark](images/aardvark.png)
--- env.ps1
+++ env.ps1
@@ -2,4 +2,3 @@ $env:Path += ";C:\MinGW\bin" # MinGW Setup
 $env:Path += ";C:\ProgramData\chocoportable" # Chocolatey
 $env:Path += ";C:\ProgramData\chocolatey\bin" # Make command
 $env:Path += ";C:\tools\git\bin" # Git
-$env:Path += ";C:\Aardvark\aardvark" # Aardvark
