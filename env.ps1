a33896699aa06c4917a466e40abe8b019e3b9c22
zplusfour <zplusfour.node@outlook.com>
2020-12-28 19:55:44 +0200

    init

--- env.ps1
+++ env.ps1
@@ -3,3 +3,4 @@ $env:Path += ";C:\ProgramData\chocoportable" # Chocolatey
 $env:Path += ";C:\ProgramData\chocolatey\bin" # Make command
 $env:Path += ";C:\tools\git\bin" # Git
 $env:Path += ";C:\Aardvark\aardvark\aardvark\bin" # Aardvark
+$env:Path += ";C:\Aardvark\aardvark\apm\apm" # APM
