e610551d9cb4c5884db84426218d7248eb7c2af4
zplusfour <zplusfour.node@outlook.com>
2020-12-30 18:31:27 +0200

    executable file fixed

--- env.ps1
+++ env.ps1
@@ -3,4 +3,4 @@ $env:Path += ";C:\ProgramData\chocoportable" # Chocolatey
 $env:Path += ";C:\ProgramData\chocolatey\bin" # Make command
 $env:Path += ";C:\tools\git\bin" # Git
 $env:Path += ";C:\Aardvark\aardvark\bin" # Aardvark
-$env:Path += ";C:\Aardvark\apm" # APM
+$env:Path += ";C:\Aardvark\apm\bin" # APM
