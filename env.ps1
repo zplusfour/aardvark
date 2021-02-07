53abe861a53489b3360656c294a199470540b4f0
zplusfour <zplusfour.node@outlook.com>
2021-02-07 15:14:38 +0200

    removed gitignore

--- env.ps1
+++
@@ -1,6 +0,0 @@
-$env:Path += ";C:\MinGW\bin" # MinGW Setup
-$env:Path += ";C:\ProgramData\chocoportable" # Chocolatey
-$env:Path += ";C:\ProgramData\chocolatey\bin" # Make command
-$env:Path += ";C:\tools\git\bin" # Git
-$env:Path += ";C:\Aardvark\aardvark\bin" # Aardvark
-$env:Path += ";C:\Aardvark\apm\bin" # APM
